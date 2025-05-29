1ea9c5253e20fe080bea08869eda201f32b47e81
clrbuilder <clrbuilder@intel.com>
2025-05-29 09:00:16 -0700

    Pundle update using 28ae772b4aca7d843a6c870b02be1ec853207edf

---
+++ bundles/paho.mqtt.c
@@ -0,0 +1,10 @@
+# [TITLE]: paho.mqtt.c
+# [DESCRIPTION]:
+# [STATUS]:
+# [CAPABILITIES]:
+# [TAGS]:
+# [MAINTAINER]: pundle
+include(paho.mqtt.c-bin)
+include(paho.mqtt.c-lib)
+include(paho.mqtt.c-license)
+paho.mqtt.c
---
+++ bundles/paho.mqtt.cpp
@@ -0,0 +1,9 @@
+# [TITLE]: paho.mqtt.cpp
+# [DESCRIPTION]:
+# [STATUS]:
+# [CAPABILITIES]:
+# [TAGS]:
+# [MAINTAINER]: pundle
+include(paho.mqtt.cpp-lib)
+include(paho.mqtt.cpp-license)
+paho.mqtt.cpp
