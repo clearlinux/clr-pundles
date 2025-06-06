033b87e59b6b227d7f73ad1ab36c67d2a6463ade
clrbuilder <clrbuilder@intel.com>
2025-06-06 10:58:16 -0700

    Pundle update using e3905c402f006df027c92858c3d4d194c121c947

---
+++ bundles/llama.cpp
@@ -0,0 +1,10 @@
+# [TITLE]: llama.cpp
+# [DESCRIPTION]:
+# [STATUS]:
+# [CAPABILITIES]:
+# [TAGS]:
+# [MAINTAINER]: pundle
+include(llama.cpp-bin)
+include(llama.cpp-lib)
+include(llama.cpp-license)
+llama.cpp
